39a5fa60b70297a296ab3fb4aa2cdbaa8b60256a
Eloi BERLINGER <eloi.berling@gmail.com>
2018-04-19 20:43:01 +0200

    Starting server configuration file developpement...

--- server/modules/server.py
+++ server/modules/server.py
@@ -1,3 +1,20 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+# Livia server class file
+import socket
+import select
+import cPickle, shelve
+
 class Server:
-    def __init__():
+    def __init__(self):
+        # Initialise server variables:
+        self.users = list()
+        self.host = str()
+        self.state = False
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Getting server configuration:
+
+
+    def start():
         pass
