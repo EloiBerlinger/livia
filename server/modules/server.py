ad96b0118da6a350e429fe0524ad963716b3b26a
Eloi BERLINGER <eloi.berling@gmail.com>
2018-04-25 21:12:35 +0200

    Little update for server configuration

--- server/modules/server.py
+++ server/modules/server.py
@@ -3,7 +3,6 @@
 # Livia server class file
 import socket
 import select
-import cPickle, shelve
 
 class Server:
     def __init__(self):
@@ -14,7 +13,30 @@ class Server:
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # Getting server configuration:
+        with open("serverConfig.conf", "r") as configFile:
 
+            extractedValues = {}
+
+            for l in configFile:
+                if(l[0] != "#"):
+                    # If the first caracter of the line is not #:
+                    def extractValue(key, line):
+                        final = ""
+                        keyLength = len(key+" = ")
+                        lineLength = len(l)
+                        for c in line:
+                            charIndex = line.index(c)
+                            if(charIndex in range(keyLength, lineLength)):
+                                final += c
+
+                        return final
+
+                    if("serverName" in l):
+                        extractedValues["serverName"] = extractValue(l, "serverName")
+                    elif("serverPort" in l):
+                        extractedValues["serverPort"] = extractValue(l, "serverPort")
+
+        print(extractedValues)
 
     def start():
         pass
