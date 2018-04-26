2884734e47e647f30dc99d880ae9738e71a650e2
Eloi BERLINGER <eloi.berling@gmail.com>
2018-04-26 21:58:27 +0200

    Added server commands

--- client/modules/commands/help.py
+++ client/modules/commands/help.py
@@ -9,6 +9,7 @@ def helpCmd(command, client):
         \x1b[0;32mClear console\x1b[0m  ->  /clear
         \x1b[0;32mLeave app\x1b[0m      ->  /exit
         \x1b[0;32mGet your info\x1b[0m  ->  /info
+        \x1b[0;32mConnect to server\x1b[0m -> /connect <serverName>
 
     \x1b[1;31mList of Domains:\x1b[0m
         \x1b[0;32mServers management\x1b[0m -> /help servers
@@ -21,34 +22,41 @@ def helpCmd(command, client):
         Clear console  ->  /clear
         Leave app      ->  /exit
         Get your info  ->  /info
+        Connect to server -> /connect <serverName>
 
     List of Domains:
-        Servers management -> /help servers
+        Servers management -> /help server
         Client configuration -> /help config
 
 """)
     else:
-        if(command[1] == "servers"):
+        if(command[1] == "server"):
             if(client.clientConfig["colorSupporting"]):
                 print("""
     \x1b[1;31mServers help:\x1b[0m
+        For precise the server port just add :port after server address !
 
+        \x1b[0;32mGet saved servers\x1b[0m -> /server list
         \x1b[0;32mAdd new server\x1b[0m ->  /server add <host> <name>
         \x1b[0;32mRemove server\x1b[0m  ->  /server del <name>
 
-        \x1b[0;32mFavorite server add\x1b[0m -> /server fav add <serverName>
-        \x1b[0;32mFavorite server del\x1b[0m -> /server fav del <serverName>
+        \x1b[0;32mGet favorite servers\x1b[0m -> /server fav list
+        \x1b[0;32mFavorite server add\x1b[0m -> /server fav add <name>
+        \x1b[0;32mFavorite server del\x1b[0m -> /server fav del <name>
 
 """)
             else:
                 print("""
     Servers help:
+        For precise the server port just add :port after server address !
 
+        Get saved servers -> /server list
         Add new server ->  /server add <host> <name>
         Remove server  ->  /server del <name>
 
-        Favorite server add -> /server fav add <serverName>
-        Favorite server del -> /server fav del <serverName>
+        Get favorite servers -> /server fav list
+        Favorite server add -> /server fav add <name>
+        Favorite server del -> /server fav del <name>
 
 """)
         elif(command[1] == "config"):
@@ -59,7 +67,7 @@ def helpCmd(command, client):
     Required:
         \x1b[0;32mYour name\x1b[0m     ->  /config name <YourUsername>
         \x1b[0;32mYour Password\x1b[0m ->  /config password <YourPassword>
-        \x1b[0;32mColor support\x1b[0m ->  /config coloredtext <On/Off>
+        \x1b[0;32mColor support\x1b[0m ->  /config color <On/Off>
 
     Facultative:
         \x1b[0;32mYour Email\x1b[0m ->  /config email <yourMail>
@@ -72,7 +80,7 @@ def helpCmd(command, client):
 
     Required:
         Your name     ->  /config name <YourUsername>
-        Your Password ->  /config password <YourPassword>
+        Your Password ->  /config password
         Color support ->  /config color <On/Off>
 
     Facultative:
--- client/modules/commands/server.py
+++ client/modules/commands/server.py
@@ -2,3 +2,78 @@
 #-*-coding:utf-8-*-
 def serverCmd(command, client):
     commandErrorMessage = "Error while using this command, for help use /help server"
+    if(len(command) == 1):
+        print(commandErrorMessage)
+    else:
+        if(command[1] == "add"):
+            if(len(command) == 4):
+                host = command[2]
+                serverName = command[3]
+                client.servers[serverName] = host
+            else:
+                print(commandErrorMessage)
+                print("If your placed spaces in the server name you can't !")
+
+        elif(command[1] == "del"):
+            if(len(command) == 3):
+                del client.servers[command[2]]
+            else:
+                print(commandErrorMessage)
+
+        elif(command[1] == "list"):
+            print("\n", end="")
+            print("serverName | serverHost | (fav)\n")
+            for s in client.servers.keys():
+                if(s in client.bestServers):
+                    if(client.clientConfig["colorSupporting"]):
+                        print("\x1b[1;32m"+s+"\x1b[0m | "+client.servers[s]+" | \x1b[1;33mFav\x1b[0m")
+                    else:
+                        print(s+" | "+client.servers[s]+" | Fav")
+                else:
+                    if(client.clientConfig["colorSupporting"]):
+                        print("\x1b[1;32m"+s+"\x1b[0m | "+client.servers[s])
+                    else:
+                        print(s+" | "+client.servers[s])
+
+            print("\n", end="")
+
+        elif(command[1] == "fav"):
+            if(command[2] == "add"):
+                serverName = command[3]
+                serverExist = False
+
+                for key in client.servers.keys():
+                    if(key == serverName):
+                        serverExist = True
+
+                if(not serverExist):
+                    print("Error > The server "+serverName+" is not existing !")
+                else:
+                    client.bestServers.append(serverName)
+
+            elif(command[2] == "del"):
+                serverName = command[3]
+                serverExist = False
+                for server in client.bestServers:
+                    if(server == serverName):
+                        serverExist = True
+
+                if(not serverExist):
+                    print("Error > The server "+serverName+" is not in your favorite servers list !")
+                else:
+                    del client.bestServers[client.bestServers.index(serverName)]
+
+            elif(command[2] == "list"):
+                print("serverName | serverHost\n")
+                for server in client.bestServers:
+                    if(client.clientConfig["colorSupporting"]):
+                        print("\x1b[1;32m"+server+"\x1b[0m | "+client.servers[server]+"")
+                    else:
+                        print(server+" | "+client.servers[server]+"")
+
+                print("\n", end="")
+
+            else:
+                print(commandErrorMessage)
+        else:
+            print(commandErrorMessage)
