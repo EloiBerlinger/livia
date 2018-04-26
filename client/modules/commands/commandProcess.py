12b31b5e46527a456c7b86933a68be3176e8587b
Eloi BERLINGER <eloi.berling@gmail.com>
2018-04-26 22:00:45 +0200

    Added connect command on the commandProcess file

--- client/modules/commands/commandProcess.py
+++ client/modules/commands/commandProcess.py
@@ -5,6 +5,7 @@ from modules.commands.config import *
 from modules.commands.server import *
 from modules.commands.clear import *
 from modules.commands.info import *
+from modules.commands.connect import *
 
 def commandProcess(command, client):
 
@@ -25,6 +26,9 @@ def commandProcess(command, client):
 
     elif(command[0] == "info"):
         infoCmd(client)
+
+    elif(command[0] == "connect"):
+        connectCmd(command, client)
 
     else:
         print("\nUnknow command, type /help\n")
